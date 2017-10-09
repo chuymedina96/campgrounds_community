33327dcd19aad299d8d2354fc688e0168b862c15
Chuy Medina <chuymedina96@gmail.com>
2017-10-09 14:07:27 -0500

    more

--- app.js
+++ app.js
@@ -66,7 +66,6 @@ app.get("*", function(req, res){
 app.listen(process.env.PORT || 3000, function(){
   console.log("Server has started!");
 });
-//google maps api key: AIzaSyDgigJTU3ssL2iSdjtoM7tZPurcgy61gN0
 //Playing around with Yahoo`s weather api
 /*app.get("/yahoo", function(req, res){
     request("https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", function(error, responce, body){
